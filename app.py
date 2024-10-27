c172f56d55b8a751369da66363e37312cbd006bb
Johanes Latupapua <merhantine@gmail.com>
2024-10-27 23:45:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def display_chat_history(chain):
 
 def create_conversational_chain(vector_store):
     llm = ChatOpenAI(
-        model_name="gpt-4-turbo",
+        model_name="gpt-4o-mini",
         temperature=0.1,
         openai_api_key=st.secrets['OPENAI_API_KEY']
     )
